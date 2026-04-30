11fb563f43dd70ec0fdda90fa6acd341fb4b634c
Kak0ytachel <alesha.belarus@gmail.com>
2026-04-30 22:52:36 +0200

    added sample websocket handlers

--- websocket.js
+++ websocket.js
@@ -1,4 +1,27 @@
+// function handle_send()
 
+/**
+ * @type {Object<string, ((conn: SocketStream, payload: Object) => Promise<void>)>}
+ */
+const HANDLERS = {
+    // /**
+    //  * @param {SocketStream} conn
+    //  * @param {Object} payload
+    //  * @constructor
+    //  */
+    START: (conn, payload) => {
+        console.log(payload);
+    },
+
+    PING: (conn, payload) => {
+        conn.send(JSON.stringify({
+            type: 'PONG',
+            payload: {
+                date: Date.now()
+            }
+        }))
+    }
+}
 
 /**
  * @param {FastifyInstance} fastify
@@ -7,11 +30,13 @@
 async function websocket_routes(fastify, options) {
     fastify.get('/websocket', { websocket: true },
         (connection /* SocketStream */, req /* FastifyRequest */) => {
-
-            console.log(connection)
+            // console.log(connection)
             connection.on('message', (message) => {
+                message = JSON.parse(message);
+                if (message.type in HANDLERS) HANDLERS[message.type](connection, message.payload)
+                else console.log('Unknown message type')
                 // message.toString() === 'hi from client'
-                connection.send('hi from server');
+                // connection.send('hi from server');
             });
         }
     );
