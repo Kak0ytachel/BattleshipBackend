e4e619f943eca398412905e959a0dc0f10e49541
Kak0ytachel <alesha.belarus@gmail.com>
2026-04-30 23:39:11 +0200

    added handling wrong websocket message type

--- websocket.js
+++ websocket.js
@@ -18,22 +18,11 @@ function send_handle(type, payload) {
  * @type {Object<string, ((conn: SocketStream, payload: Object) => Promise<void>)>}
  */
 const HANDLERS = {
-    // /**
-    //  * @param {SocketStream} conn
-    //  * @param {Object} payload
-    //  * @constructor
-    //  */
     START: (conn, payload) => {
         console.log(payload);
     },
 
     PING: (conn, payload) => {
-        // conn.send(JSON.stringify({
-        //     type: 'PONG',
-        //     payload: {
-        //         date: Date.now()
-        //     }
-        // }))
         conn.send_handle("PONG", {
             date: Date.now()
         })
@@ -46,16 +35,20 @@ const HANDLERS = {
  */
 async function websocket_routes(fastify, options) {
     fastify.get('/websocket', { websocket: true },
-        (connection /* SocketStream */, req /* FastifyRequest */) => {
-            // connection.send_handle = (...args) => {console.log(args); s};
+        /**
+         * @param {SocketStream} connection
+         * @param {FastifyRequest} req
+         */
+        (connection, req) => {
             connection.send_handle = send_handle;
-            // console.log(connection)
             connection.on('message', (message) => {
                 message = JSON.parse(message);
-                if (message.type in HANDLERS) HANDLERS[message.type](connection, message.payload)
-                else console.log('Unknown message type')
-                // message.toString() === 'hi from client'
-                // connection.send('hi from server');
+                const { type, payload } = message;
+                if (type in HANDLERS) HANDLERS[type](connection, payload)
+                else {
+                    console.log('Unknown message type')
+                    connection.send_handle("ERROR", {"error": "Unknown message type"})
+                }
             });
         }
     );
