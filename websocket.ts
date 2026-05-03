69021bb0f7ca85130b7c09363dd9e5f6176da683
Kak0ytachel <alesha.belarus@gmail.com>
2026-05-03 11:47:18 +0200

    implemented WebSocket authorization via jwt ticket from http request

--- websocket.ts
+++ websocket.ts
@@ -11,29 +11,52 @@ function send_handle(this: WebSocket, type: string, payload: Object) {
     )
 }
 
-const HANDLERS: { [key: string]: (conn: WebSocket & { send_handle: typeof send_handle }, payload: Object) => void } = {
-    START: (conn, payload) => {
+const HANDLERS: { [key: string]: (conn: WebSocket & { send_handle: typeof send_handle }, payload: Object, user_id: number) => void } = {
+    START: (conn, payload, user_id) => {
         console.log(payload);
     },
 
-    PING: (conn, payload) => {
+    PING: (conn, payload, user_id) => {
         conn.send_handle("PONG", {
-            date: Date.now()
+            date: Date.now(),
+            user_id: user_id
         })
     }
 }
 
 async function websocket_routes(fastify: FastifyInstance, options: Object) {
-    fastify.get('/websocket', { websocket: true },
+    fastify.get<{Querystring: {ticket?: string}}>('/websocket', { websocket: true },
 
         (connection: any, req) => {
+
             connection = connection as WebSocket & { send_handle: typeof send_handle }
             connection.send_handle = send_handle;
+
+            const ticket = req.query.ticket ?? "";
+            if (ticket === "") {
+                connection.close();
+                connection.send_handle("ERROR", {"error": "No ticket provided"})
+                return
+            }
+            let payload: { user_id: number }
+            try {
+                payload = fastify.jwt.verify(ticket);
+            }
+            catch (e) {
+                fastify.log.error(e);
+                connection.send_handle("ERROR", {"error": "Invalid ticket"})
+                connection.close();
+                return
+            }
+            const user_id = payload.user_id;
+            console.log("user_id ", user_id);
+
+            // console.log("headers ", connection.headers);
             connection.on('message', (message: string) => {
                 const messageObject: {type: string, payload: Object} = JSON.parse(message);
                 const { type, payload } = messageObject;
                 if (type in HANDLERS) {
-                    HANDLERS[type]?.(connection, payload)
+                    HANDLERS[type]?.(connection, payload, user_id)
                 }
                 else {
                     console.log('Unknown message type')
@@ -43,6 +66,18 @@ async function websocket_routes(fastify: FastifyInstance, options: Object) {
             connection.send_handle("HELLO", {})
         }
     );
+
+    fastify.get('/websocket-auth', (connection: any, req) => {
+        console.log("headers ", connection.headers);
+        const token = connection.headers.authorization?.split(" ")[1] ?? "";
+        if (token === "") {
+            return {error: "No token provided"}
+        }
+        const payload: { user_id: string } = fastify.jwt.verify(token);
+        const user_id = payload.user_id;
+        const ticket = fastify.jwt.sign({ user_id, type: "ticket" }, { expiresIn: '10m' })
+        return {ticket: ticket};
+    })
 }
 
 export default websocket_routes;
