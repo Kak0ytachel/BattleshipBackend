5d58b7e6808e703f2e94c26d393251cdf0626e1d
Kak0ytachel <alesha.belarus@gmail.com>
2026-05-29 20:29:55 +0200

    logging db notices added

--- postgres-connector.ts
+++ postgres-connector.ts
@@ -6,6 +6,18 @@ async function dbConnector (fastify: FastifyInstance, options: Object) {
     fastify.register(fastifyPostgres, {
         connectionString: 'postgres://postgres@localhost:5432/postgres?password=1111&options=-csearch_path=battleship,public'
     }) // TODO: move password to env variable
+
+    fastify.ready((err) => {
+        if (err) throw err
+
+        // Access the underlying node-postgres Pool instance
+        fastify.pg.pool.on('connect', (client) => {
+            // Listen for notices sent to this specific client
+            client.on('notice', (msg) => {
+                fastify.log.info({ pgNotice: msg }, `PostgreSQL Notice: ${msg.message}`)
+            })
+        })
+    })
 }
 
 // Wrapping a plugin function with fastify-plugin exposes the decorators
